89ac6b2505caac58e79c9ca7f82353324041b641
HUCORP <53086460+HUCORP-admin@users.noreply.github.com>
2023-02-20 14:18:56 -0500

    Delete kenney's tiles from root

--- Kenney Topdown Shooter Pack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Kenney Topdown Shooter Pack" tilewidth="64" tileheight="64" spacing="10" tilecount="540" columns="27">
- <image source="../Game/assets/spritesheet_tiles.png" width="1988" height="1470"/>
-</tileset>
